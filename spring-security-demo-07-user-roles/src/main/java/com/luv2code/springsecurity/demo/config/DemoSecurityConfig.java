08b3641cce3c9b25e98e0fb3783a92cfd0e85681
iamudachan <47654533+iamudachan@users.noreply.github.com>
2019-03-14 12:14:06 +0530

    Add files via upload

---
+++ spring-security-demo-07-user-roles/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+
+@Configurable
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		auth.inMemoryAuthentication().withUser(users.username("john").password("test123").roles("EMPLOYEE","ADMIN"));
+		auth.inMemoryAuthentication().withUser(users.username("kiran").password("test123").roles("EMPLOYEE","MANAGER"));
+		auth.inMemoryAuthentication().withUser(users.username("mahesh").password("test123").roles("EMPLOYEE"));
+	
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	
+		http.authorizeRequests()
+//			.anyRequest().authenticated()
+			.antMatchers("/").hasRole("EMPLOYEE")
+			.antMatchers("/leaders/**").hasRole("MANAGER")
+			.antMatchers("/systems/**").hasRole("ADMIN")
+			.and()
+			.formLogin().loginPage("/showLoginPage")
+			.loginProcessingUrl("/authonticateTheUser").permitAll()
+			.and().logout().permitAll()
+			.and()
+			.exceptionHandling().accessDeniedPage("/access-denied");
+	}
+}
